dd9c8101db80765e186c6233dcfc36af9543abc4
Aishani2002 <aishani.acharya@gmail.com>
2021-01-23 18:38:28 +0530

    stan

---
+++ Standard.py
@@ -0,0 +1,31 @@
+import csv
+import pandas as pd
+import math
+
+with open("data.csv", newline="") as f:
+    reader= csv.reader(f)
+    file_data= list(reader)
+
+data= file_data[0]
+    
+def mean(data):
+    n= len(data)
+    total=0
+    for x in data:
+        total += int(x)
+    mean= total/n
+    return mean
+
+squaredList= []
+for num in data:
+    a=int(num)-mean(data)
+    a= a**2
+    squaredList.append(a)
+
+sum=0
+for i in squaredList:
+    sum= sum+i
+    
+result= sum/(len(data)-1)
+standardDeviation= math.sqrt(result)
+print(standardDeviation)
